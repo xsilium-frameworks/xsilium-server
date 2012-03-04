dabe64e1848042a3bdaeddb8d05e05fff6828f1a
Joda <johan.lore@free.fr>
2012-03-04 20:59:53 +0100

    Mise a jour des commentaires

--- src/server/Shared/Structure/Server.h
+++ src/server/Shared/Structure/Server.h
@@ -21,7 +21,6 @@ typedef struct AUTH_LOGON_PROOF_S
 {
     uint8_t   cmd; /*!< correspond au type de packet que l'on envoi */
     int   error; /*!< correspond a l'erreur qui a ete detecter */
-    int   M2[20]; /*      */
 } sAuthLogonProof_S;
 
 
@@ -33,17 +32,16 @@ typedef struct sClient
 {
 	RakNet::RakNetGUID 	guid; /*!< Numero unique generer lors de la connection au socket  */
 	std::string IP ; /*!< L'ip du client       */
-    uint8_t idLogin; /*      */
-    uint16_t  build; /*      */
-    uint8_t   platform[4]; /*      */
+    uint8_t idLogin; /*!< numero unique du compte dans la base de donnŽe      */
+    uint16_t  build; /*!< version du client      */
+    uint8_t   platform[4]; /* la platform du client      */
     uint8_t   os[4]; /*      */
-    uint8_t   country[4]; /*      */
-    std::string shaPassHash; /*      */
-    bool locked; /*      */
+    uint8_t   country[4]; /* le pays du client      */
+    std::string shaPassHash; /* Le mot de passe crypter */
+    bool locked; /* Si le client lock sont compte sur l'ip*/
     std::string lastIP; /*!< derniere IP du compte  */
-    uint8_t gmlevel[50] ; /*      */
-    std::string shaPassHask; /*      */
-    int accountUnBanDate ; /*      */
+    uint8_t gmlevel[50] ; /*Niveau de MJ*/
+    int accountUnBanDate ; /*si le compte est bannie date de debanne du compte*/
 };
 
 
