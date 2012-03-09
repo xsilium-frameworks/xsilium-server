a477ef64c0774a8a5d3a5b380fb418e157e64640
mathieu <mathieu@Atlantis>
2012-03-09 01:47:31 +0100

    ajout de doc

--- src/server/Auth/AuthServer.h
+++ src/server/Auth/AuthServer.h
@@ -1,8 +1,15 @@
 /*
  * authServer.h
  *
- *  Created on: 2 fŽvr. 2012
- *      Author: joda2
+ *  Created on: 2 fï¿½vr. 2012
+ *      Author:
+ */
+
+/*!
+ * \file AuthServer.h
+ * \brief authentication server
+ * \author joda2
+ *
  */
 
 #ifndef AUTHSERVER_H_
@@ -12,13 +19,47 @@
 
 #include "Authentification/Authentification.h"
 
+  /*! \class CPlayer
+   * \brief classe permettant la gestion de l'authentification
+   *
+   *  La classe gÃ¨re l'authentification des clients
+   */
 
 class authServer {
 public:
+
+    /*!
+     *  \brief Constructeur
+     *
+     *  Constructeur de la classe authServer
+     *
+     *  \param
+     */
+
 	authServer();
+
+    /*!
+     *  \brief Destructeur
+     *
+     *  Destructeur de la classe ~authServer
+     */
+
 	virtual ~authServer();
 
+    /*!
+     *  \brief StartThread
+     *
+     *  Launches starThread
+     */
+
 	void startThread();
+
+    /*!
+     *  \brief StopThread
+     *
+     *  Kill the Tread
+     */
+
 	void stopThread();
 
 private:
