87f46094bade3e13cedfb434d6416352e2d8a153
mathieu <mathieu@Atlantis>
2012-03-21 14:53:15 +0100

    Incomplete - task 120: Documentation http://gestion.snowsuprem.eu/issues/show/120

--- src/server/Auth/Authentification/Authentification.h
+++ src/server/Auth/Authentification/Authentification.h
@@ -1,9 +1,18 @@
 /*
  * Authentification.h
  *
- *  Created on: 7 fŽvr. 2012
+ *  Created on: 7 fï¿½vr. 2012
  *      Author: joda2
  */
+
+/*!
+ * \file Authentification.h
+ * \brief authentication
+ * \author joda2
+ *
+ */
+
+
 #ifndef AUTHENTIFICATION_H_
 #define AUTHENTIFICATION_H_
 
@@ -27,11 +36,60 @@ using namespace std;
 
 class Authentification {
 public:
+
+
+    /*!
+     *  \brief Constructeur
+     *
+     *  Constructeur de la classe Authentification
+     *	Et se connecte a la DB
+     *  \param
+     */
+
 	Authentification();
+
+    /*!
+     *  \brief Constructeur
+     *
+     *  Destructeur de la classe Authentification
+     *  Et se dÃ©connecte de la DB
+     *  \param
+     */
+
 	virtual ~Authentification();
 
+    /*!
+     *  \brief CreateClient
+     *
+     *  Prend le client et le met dans la pile
+     *  gestion de la pile FIFO
+     *  Et l'integre dans la DB
+     *
+     *  \param
+     */
+
 	bool CreateClient(Packet *packet);
+
+    /*!
+     *  \brief DeleteClient
+     *
+     *  Prend le client et le met dans la pile
+     *  gestion de la pile FIFO
+     *  Et supprime de la DB
+     *
+     *  \param
+     */
+
 	bool DeleteClient(Packet *packet);
+
+    /*!
+     *  \brief FindClient
+     *
+     *  Recherche le client dans la DB
+     *
+     *  \param
+     */
+
 	bool FindClient(RakNetGUID guid);
 
     bool _HandleLogonChallenge( RakNet::Packet *packet);
