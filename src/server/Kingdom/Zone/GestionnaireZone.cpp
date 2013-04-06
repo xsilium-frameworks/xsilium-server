ec61309e9cc88cc56c22a03360ae34e991aac61c
Joda <johan.lore@free.fr>
2013-04-07 00:11:30 +0200

    Incomplete - task references #157: gestionnaireDeZone

---
+++ src/server/Kingdom/Zone/GestionnaireZone.cpp
@@ -0,0 +1,19 @@
+/*
+ * \file GestionnaireZone.cpp
+ *
+ *  Created on: \date 7 avr. 2013
+ *      Author: \author joda
+ *  \brief :
+ */
+
+#include "GestionnaireZone.h"
+
+GestionnaireZone::GestionnaireZone() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GestionnaireZone::~GestionnaireZone() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/server/Kingdom/Zone/GestionnaireZone.h
@@ -0,0 +1,21 @@
+/*
+ * \file GestionnaireZone.h
+ *
+ *  Created on: \date 7 avr. 2013
+ *      Author: \author joda
+ *  \brief :
+ */
+
+#ifndef GESTIONNAIREZONE_H_
+#define GESTIONNAIREZONE_H_
+
+/*
+ *
+ */
+class GestionnaireZone {
+public:
+	GestionnaireZone();
+	virtual ~GestionnaireZone();
+};
+
+#endif /* GESTIONNAIREZONE_H_ */
