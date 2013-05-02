74d46cfe7a4438742489b3355f935233227b5f68
kiti <diane.elkaim@gmail.com>
2013-05-02 17:54:00 +0200

    NOUVEAU - task references #181: ecrireMsg

---
+++ src/server/Kingdom/Mail/Mail.h
@@ -0,0 +1,41 @@
+/*
+ * Mail.h
+ *
+ *  Created on: 1 mai 2013
+ *      Author: kiti
+ *      Gestion des mails
+ */
+
+#ifndef MAIL_H_
+#define MAIL_H_
+
+#include "ModuleActif/ModuleActif.h"
+#include "Session/GestionnaireSession.h"
+#include "StructurePacket/StructureMail.h"
+
+enum typeMail
+{
+	MAIL_S = 0,		//Mail simple,sans pièces jointes
+	MAIL_PJ = 1,	//Mail avec pièces Jointes
+	MAIL_CR = 2,	//Mail Contre remboursement
+
+};
+
+
+class Mail {
+public:
+	Mail(Connexion * connexion);
+	virtual ~Mail();
+
+	void run();
+
+	void stopThread();
+
+	void sendMail(sMailPacket_C  *data); //gerrer la gestion de la suppression des items dans l'inventaire de la source ? via cpp ?
+
+private:
+	static void  threadMail(void * arguments);
+	GestionnaireSession * gestionnaireSession ;
+};
+
+#endif /* MAIL_H_ */
