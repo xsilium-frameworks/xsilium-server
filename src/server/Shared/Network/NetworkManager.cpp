e7274adf443cbbd77dbc61411b0476515a2956da
Joda <johan.lore@free.fr>
2014-10-19 14:33:35 +0200

    remise a plat du serveur

---
+++ src/server/Shared/Network/NetworkManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * \file NetworkManager.cpp
+ *
+ *  Created on: \date 12 oct. 2014
+ *      Author: \author joda
+ *  \brief :
+ */
+#include "NetworkManager.h"
+
+NetworkManager::NetworkManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NetworkManager::~NetworkManager() {
+	// TODO Auto-generated destructor stub
+}
+
