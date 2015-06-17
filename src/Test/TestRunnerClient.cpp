6c67c740a9b53f7c07a66ec4b26480fab28b718c
Joda <johan.lore@free.fr>
2015-06-17 23:13:34 +0200

    Incomplete - task references #258: Replacing the test library cppunit by Boost Test Library

--- src/Test/TestRunnerClient.cpp
+++ src/Test/TestRunnerClient.cpp
@@ -1,8 +1,3 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_DETECT_MEMORY_LEAK 1
-#define BOOST_TEST_LOG_FORMAT XML
-#define BOOST_TEST_LOG_LEVEL ALL
-#define BOOST_TEST_REPORT_FORMAT NO
-#define BOOST_TEST_LOG_SINK "boost-report.xml"
 #include <boost/test/unit_test.hpp>
