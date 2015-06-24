9afaf62091d69cea88153b99a61baf2ae3f754b0
Joda <johan.lore@free.fr>
2015-06-24 22:37:59 +0200

    Incomplete - task references #258: Replacing the test library cppunit by Boost Test Library

---
+++ script/jenkinsTest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WORKSPACE=${1}
+BUILD=$WORKSPACE"/build/"
+REPORT=$WORKSPACE"/report/"
+
+echo "Lancement des tests unitaire "
+
+cd $BUILD
+
+cp $WORKSPACE"/bin/test/TestConfig.config" .
+
+make UnitTester_coverage
+mv *.xml $REPORT
+
+cppcheck $WORKSPACE"/src" --enable=all --quiet --xml 2> $REPORT"cppcheck-result.xml"
+
+
+
+
