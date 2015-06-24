fee454a584de92d5068396ab8027dbf7ae34fd68
Skronak <Skronak@Narvak.home>
2015-06-24 22:50:36 +0200

    Incomplete - task references #262: Creation of Chat.h & Chat.cpp skeleton

--- src/Server/Kingdom/Chat/Chat.cpp
+++ src/Server/Kingdom/Chat/Chat.cpp
@@ -9,13 +9,84 @@
 
 namespace Kingdom {
 
-Chat::Chat() {
-	// TODO Auto-generated constructor stub
-
+Chat::Chat(NetworkManager * networkManager) {
+	this->networkManager = networkManager;
+	config = Configuration::getInstance();
+	log = Log::getInstance();
 }
 
 Chat::~Chat() {
-	// TODO Auto-generated destructor stub
+	networkManager->removeListenneur(ID_CHAT);
 }
 
-} /* namespace Kingdom */
+void Chat::run()
+{
+	networkManager->addListenneur(ID_CHAT,this);
+	NetworkListener::run();
+}
+
+void Chat::processPacket(MessageNetwork * messageNetwork)
+{
+	log->write(Log::DEBUG,"Nouveau Packet de Chat");
+	MessagePacket * messageRetour = new MessagePacket();
+	switch(messageNetwork->messagePacket->getSousOpcode())
+	{
+	case ID_WORLD :
+		HandleChatWorld(messageNetwork,messageRetour);
+		break;
+	case ID_AREA :
+		HandleChatArea(messageNetwork,messageRetour);
+		break;
+	case ID_TRADE :
+		HandleChatTrade(messageNetwork,messageRetour);
+		break;
+	case ID_PRIVATE :
+		HandleChatPrivate(messageNetwork,messageRetour);
+		break;
+	default:
+		break;
+	}
+	networkManager->sendPacket(messageNetwork->session->getSessionPeer(),0,messageRetour);
+}
+
+int  Chat::HandleChatPrivate(MessageNetwork * messageNetwork,MessagePacket * messageRetour)
+{
+	// Controle presence donnée identifiant du destinataire
+	if(!messageNetwork->messagePacket->hasProperty("id_dest"))
+	{
+		log->write(Log::INFO,"Le message venant de %d:%d est illisible ",messageNetwork->session->getSessionID()->host,messageNetwork->session->getSessionID()->port);
+		messageRetour->setOpcode(ID_AUTH);
+		messageRetour->setSousOpcode(ID_ERREUR);
+		messageRetour->setProperty("ErrorId",ID_ERROR_PACKET_SIZE);
+		return ID_ERROR_PACKET_SIZE ;
+	}
+
+	messageRetour->setOpcode(ID_CHAT);
+	messageRetour->setSousOpcode(ID_PRIVATE);
+	messageRetour->setProperty("Key",1234567);
+
+	return ID_NOERROR;
+}
+
+// envoi à cette personne
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+/* namespace Kingdom */
--- src/Server/Kingdom/Chat/Chat.h
+++ src/Server/Kingdom/Chat/Chat.h
@@ -5,19 +5,52 @@
  *      Author: \author joda
  *  \brief :
  */
+#include <Network/NetworkManager.h>
+#include <Configuration/Configuration.h>
+
 #ifndef SRC_SERVER_KINGDOM_CHAT_CHAT_H_
 #define SRC_SERVER_KINGDOM_CHAT_CHAT_H_
 
 namespace Kingdom {
+	enum typeForChat
+	{
+		ID_WORLD,
+		ID_AREA,
+		ID_TRADE,
+		ID_PRIVATE,
+		ID_ERREUR
+	};
+
+	enum erreurOfChat
+	{
+		ID_NOERROR = 0,
+		ID_ERROR_PACKET_SIZE,
+		ID_SPAM
+	};
 
 /*
  *
  */
-class Chat {
+class Chat : public NetworkListener{
+
 public:
-	Chat();
+	Chat(NetworkManager * NetworkManager);
 	virtual ~Chat();
+	void run();
+
+	int HandleChatWorld(MessageNetwork * messageNetwork,MessagePacket * messageRetour);
+	int HandleChatArea(MessageNetwork * messageNetwork,MessagePacket * messageRetour);
+	int HandleChatTrade(MessageNetwork * messageNetwork,MessagePacket * messageRetour);
+	int HandleChatPrivate(MessageNetwork * messageNetwork,MessagePacket * messageRetour);
+	void processPacket(MessageNetwork * messageNetwork);
+
+
+private:
+	Log * log;
+	Configuration * config ;
+	NetworkManager * networkManager;
 };
+
 
 } /* namespace Kingdom */
 
