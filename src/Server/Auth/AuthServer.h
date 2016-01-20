c8fb8ac004d11e72f6e303fb1c4e4177e2dd524d
xelfe <xelfes@gmail.com>
2016-01-20 08:24:07 -0500

    EN COURS - task references #276: Doxygen doc integration

--- src/Server/Auth/AuthServer.h
+++ src/Server/Auth/AuthServer.h
@@ -1,9 +1,8 @@
-/*
- * \file AuthServer.h
- *
- *  Created on: \date 18 déc. 2014
- *      Author: \author joda
- *  \brief :
+/** \file AuthServer.h
+ *  \date 18 déc. 2014
+ *  \author xsilium-frameworks team
+ *  \author joda
+ *  \brief : Basic authentification server
  */
 #ifndef SRC_SERVER_AUTH_AUTHSERVER_H_
 #define SRC_SERVER_AUTH_AUTHSERVER_H_
@@ -16,18 +15,36 @@
 
 #include "Royaumes/RoyaumesManager.h"
 
+/** \namespace Auth
+ *
+ * namespace that contain authentification's stuff.
+ */
 namespace Auth {
 
-/*
- *
+/** \class AuthServer
+ *  \brief Class that manage the authentification server.
  */
 class AuthServer {
 public:
+	/** \brief Constructor
+	 * 
+	 * Constructor of the AuthServer class.
+	 */
 	AuthServer();
+	/** \brief Destructor
+	 * Destructor of the AuthServer class.
+	 */
 	virtual ~AuthServer();
-
+	/** \brief Start
+	 * 
+	 * Start the authentification server.
+	 */
 	void startServer();
-	void stopThread();
+	/** \brief Stop
+	 * 
+	 * Stop the authentification server.
+	 */
+	void stopThread(); /** Should change to stopServer() */
 
 
 private:
